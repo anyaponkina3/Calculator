1f58bd73f5ffa25ea9699f337fedb8d45c07554c
KKseniaK <142303908+KKseniaK@users.noreply.github.com>
2024-09-24 20:24:20 +0500

    Update translations.js

--- index_files/translations.js
+++ index_files/translations.js
@@ -7,7 +7,7 @@ const translations = {
       calculateButton: "Вычислить",
       helpTitle: "Поддержка",
       number_value: "Например, 25",
-      supportButton: "Поддержка" // Перевод для кнопки поддержки
+      supportButton: "Поддержка" 
   },
   en: {
       title: "Root Calculator",
@@ -16,7 +16,7 @@ const translations = {
       calculateButton: "Calculate",
       helpTitle: "Help",
       number_value: "For example, 25",
-      supportButton: "Support" // Перевод для кнопки поддержки
+      supportButton: "Support" 
   },
   zh: {
       title: "根计算器",
@@ -25,7 +25,7 @@ const translations = {
       calculateButton: "计算",
       helpTitle: "帮助",
       number_value: "例如，25",
-      supportButton: "支持" // Перевод для кнопки поддержки
+      supportButton: "支持" 
   },
   es: {
       title: "Calculadora de raíces",
@@ -34,7 +34,7 @@ const translations = {
       calculateButton: "Calcular",
       helpTitle: "Ayuda",
       number_value: "Por ejemplo, 25",
-      supportButton: "Soporte" // Перевод для кнопки поддержки
+      supportButton: "Soporte" 
   }
 };
 
@@ -46,14 +46,8 @@ function translatePage(language) {
   document.getElementById("number-label").innerText = translation.numberLabel;
   document.getElementById("precision-label").innerText = translation.precisionLabel;
   document.getElementById("calculate-button").innerText = translation.calculateButton;
-  
-  // Перевод текста кнопки помощи
   document.getElementById("help-title").innerText = translation.helpTitle;
-  
-  // Перевод текста кнопки поддержки
   document.getElementById("help-button").innerText = translation.supportButton;
-
-  // Перевод плейсхолдера в поле ввода
   document.getElementById("number").placeholder = translation.number_value;
 }
 
@@ -62,17 +56,15 @@ document.getElementById("language-select").addEventListener("change", function()
   const selectedLanguage = this.value;
   
   if (selectedLanguage === "other") {
-    // Перенаправление на страницу с выбором других языков
-    window.location.href = "https://t.me/TechnicalSupportCalculatorBot"; // Замените на нужную ссылку
+    window.location.href = "https://t.me/TechnicalSupportCalculatorBot"; 
   } else {
     translatePage(selectedLanguage);
   }
 });
 
-// Устанавливаем функцию обработки кнопки после загрузки библиотек
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#calculate-button").addEventListener("click", calculateRoot);
   
-  // Переводим на начальном этапе
+
   translatePage(document.getElementById("language-select").value);
 });
